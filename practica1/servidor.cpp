e32de4db018e6cdbf012726766f19b84bb06ffd6
motoep094-design <motoep094@gmail.com>
2026-03-14 22:01:55 -0500

    Libreria adicionada

--- practica1/servidor.cpp
+++ practica1/servidor.cpp
@@ -8,52 +8,52 @@
 using namespace std;
 
 int main() {
-    // Crear tuberías (si ya existen, no pasa nada)
+    
     mkfifo("/tmp/tuberia_peticion", 0666);
     mkfifo("/tmp/tuberia_respuesta", 0666);
     
-    cout << "🚀 Servidor esperando peticiones..." << endl;
+    cout << "Servidor esperando peticiones..." << endl;
     
-    // Cargar tabla hash
+    
     long long tablaHash[TAMANO_TABLA];
     ifstream archivoTabla("tabla_hash.dat", ios::binary);
     if (!archivoTabla.is_open()) {
-        cout << "❌ No encuentro tabla_hash.dat" << endl;
+        cout << "No encuentro tabla_hash.dat" << endl;
         return 1;
     }
     archivoTabla.read((char*)tablaHash, sizeof(long long) * TAMANO_TABLA);
     archivoTabla.close();
     
-    // Abrir archivo de viajes
+    
     ifstream archivoViajes("viajes.dat", ios::binary);
     if (!archivoViajes.is_open()) {
-        cout << "❌ No encuentro viajes.dat" << endl;
+        cout << "No encuentro viajes.dat" << endl;
         return 1;
     }
     
     while (true) {
-        // Abrir tubería de petición para LEER
+        
         int fd_peticion = open("/tmp/tuberia_peticion", O_RDONLY);
         int pulABuscar;
         read(fd_peticion, &pulABuscar, sizeof(int));
         close(fd_peticion);
         
         if (pulABuscar == 0) {
-            cout << "👋 Servidor terminado" << endl;
+            cout << "Servidor terminado" << endl;
             break;
         }
         
-        cout << "📥 Buscando PULocationID: " << pulABuscar << endl;
+        cout << "Buscando PULocationID: " << pulABuscar << endl;
         
-        // Calcular hash y buscar
+        
         int hash = pulABuscar % TAMANO_TABLA;
         long long offset = tablaHash[hash];
         
-        // Abrir tubería de respuesta para ESCRIBIR
+        
         int fd_respuesta = open("/tmp/tuberia_respuesta", O_WRONLY);
         
         if (offset == -1) {
-            // Enviar marca de "no encontrado"
+            
             Registro vacio;
             vacio.pulID = -1;
             write(fd_respuesta, &vacio, sizeof(Registro));
@@ -70,11 +70,11 @@ int main() {
                 }
                 offset = r.offsetSiguiente;
             }
-            // Marca de fin
+            /
             Registro fin;
             fin.pulID = -1;
             write(fd_respuesta, &fin, sizeof(Registro));
-            cout << "📤 Enviados " << encontrados << " registros" << endl;
+            cout << "Enviados " << encontrados << " registros" << endl;
         }
         close(fd_respuesta);
     }
